db27a4b3dd4875e44bb035336471ec8fd591f97c
Simon Risk <simonnrisk@gmail.com>
2025-11-23 22:24:48 -0500

    fix: bump trivia blob z score

--- src/components/TriviaBlob.tsx
+++ src/components/TriviaBlob.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export const TriviaBlob = () => {
   return (
-    <div className="absolute top-1/3 left-0 z-50 w-16 h-16 p-2 pl-4 rounded-lg shadow-lg">
+    <div className="absolute top-1/3 left-0 z-30 w-16 h-16 p-2 pl-4 rounded-lg shadow-lg">
       <Image src="/images/nerd.png" alt="Trivia Blob" width={64} height={64} />
     </div>
   );
