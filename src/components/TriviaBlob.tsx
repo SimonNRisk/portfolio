fc2946d13eaec80ea60227145f83486c324820c1
Simon Risk <simonnrisk@gmail.com>
2025-11-24 15:00:23 -0500

    chore: hover for nerd emoji

--- src/components/TriviaBlob.tsx
+++ src/components/TriviaBlob.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 
 export const TriviaBlob = ({ onClick }: { onClick: () => void }) => {
   return (
-    <div className="absolute top-1/3 left-0 z-30 w-16 h-16 p-2 pl-4 rounded-lg shadow-lg" onClick={onClick}>
+    <div
+      className="absolute top-1/3 left-0 z-30 w-16 h-16 p-2 pl-4 rounded-lg shadow-lg hover:cursor-pointer hover:scale-125 transition-transform duration-300 ease-in-out"
+      onClick={onClick}
+    >
       <Image src="/images/nerd.png" alt="Trivia Blob" width={64} height={64} />
     </div>
   );
